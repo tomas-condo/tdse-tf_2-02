33cc680718bd02735b5cff37f991c30426bcf5b2
Fran4numeros <134017014+Fran4numeros@users.noreply.github.com>
2025-12-08 19:42:26 -0300

    Update task_gameplay.c

--- app/src/task_gameplay.c
+++ app/src/task_gameplay.c
@@ -1,1 +1,16 @@
+/********************** inclusions *******************************************/
+/* Project includes */
+#include "main.h"
 
+/* Demo includes */
+#include "logger.h"
+#include "dwt.h"
+
+/* Application & Tasks includes */
+#include "board.h"
+#include "app.h"
+#include "task_sensor_attribute.h"
+#include "task_menu_attribute.h"
+#include "task_menu_interface.h"
+
+/********************** macros and definitions *******************************/
